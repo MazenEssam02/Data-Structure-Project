e619a542c42e07a55e1a5d5fcf97f8d6fbdeaef1
Mazen Essam <126350118+MazenEssam02@users.noreply.github.com>
2023-12-29 00:46:45 +0200

    Update MainFiles
    xmlToJson Added

--- MainFiles.java
+++ MainFiles.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Stack;
 public class MainFiles {
-    private ArrayList<String>data= new ArrayList<String>();
+    private ArrayList<String>data;
     private String StringData;
 
     private Stack<String> undo=new Stack<>();
@@ -33,12 +33,12 @@ public class MainFiles {
         result[2]=-1;
         result[3]=-1;
 
-        Stack<String> u = new Stack<String>();
-        Stack<String> c = new Stack<String>();
-        Stack<String> d = new Stack<String>();
-        Stack<Integer> OpenLineNumber = new Stack<Integer>();
-        Stack<Integer> tempLineNumber = new Stack<Integer>();
-        Stack<Integer> CloseLineNumber = new Stack<Integer>();
+        Stack<String> u = new Stack<>();
+        Stack<String> c = new Stack<>();
+        Stack<String> d = new Stack<>();
+        Stack<Integer> OpenLineNumber = new Stack<>();
+        Stack<Integer> tempLineNumber = new Stack<>();
+        Stack<Integer> CloseLineNumber = new Stack<>();
         boolean closing = false;
         int line = 0;
         String close = "\0";
@@ -50,9 +50,7 @@ public class MainFiles {
             for (int f = 0; f < dataChars.length; f++) {
                 String open = "";
                 close = "";
-                if (dataChars[f] == ' ') {
-
-                }
+                if (dataChars[f] == ' ') {}
 
                 else if (dataChars[f] == '<') {
 
@@ -159,11 +157,12 @@ public class MainFiles {
 
     }
 
-    //Correct Errors in XML and return the corrected one in a String
+
     //----------------------------------------------------------
+    //Correct Errors in XML and return the corrected one in a String
     public String correction(){
         undo.push(this.StringData);
-        ArrayList<String> corrected= new ArrayList<String>();
+        ArrayList<String> corrected= new ArrayList<>();
         int OpenLine = 0;
         int CloseLine=0;
         String OTag="";
@@ -212,38 +211,221 @@ public class MainFiles {
 
         return this.StringData;
     }
-    //----------------------------------------------------------
-    public String Minify()
-    {
-        undo.push(this.StringData);
-        String s=this.StringData;
-        String x = "";
-        s=s.replaceAll(">\n",">");
-        s=s.replaceAll("\n<","<");
-        s=s.replaceAll("\n","");
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)!=' '){
-                x += s.charAt(i);
-            }
-        }
-        this.StringData=x;
 
-        return x;
+    // XML to JSON Converter
+    //----------------------------------------------------------
+    public String xmlToJson() {
+
+        String s =Minify();
+
+        String json = "{";
+        String w, word = "";
+        //i for xml length, j for json length, x for repeated tags
+        int i = 0, j = 1, x = 0;
+        //tag for opening tags, but they are popped when they are closed,   brackets for({,])
+        Stack<String> stack_open_tags = new Stack<>(), tag = new Stack<>(), brackets = new Stack<>();
+        Stack<Boolean> opening_tag = new Stack<>(); // true if opening tag,and false if closing
+        //bracket holds last printed bracket
+        String last_closed_tag = "", bracket = "";
+        //saves value of x (repeated tags)
+        Stack<Integer> ind = new Stack<>();
+        while (i < s.length()) {
+
+            //iterate over the whole xml file
+            w = s.substring(i, i + 1);
+
+            if (w.equals("\r") || w.equals("\n")) {
+            }//checking if it's tag or data to be printed
+            if (w.equals("<")) {        //if it's a tag
+                i++;
+                w = s.substring(i, i + 1);
+                if (!w.equals("/")) {           //if it is opening tag
+                    while (!w.equals(">")) {    //store this tag in word
+                        word += w;
+                        i++;
+                        w = s.substring(i, i + 1);
+                    }
+                    // Handle repeated tags and adjust JSON structure accordingly
+                    //if more than one <topic> repeated in scope of <topics>, delete topic from json and add [
+                    if (word.equals(last_closed_tag)) {
+                        json = json.substring(0, x - ((brackets.size() + 1)) - 2) + json.substring(x - ((brackets.size() + 1)) - 2 + word.length() + 6 + ((brackets.size() + 1)));   //6 bec ({ \n " " : ), x->"    //remove {\n__"user":__
+                        j -= (word.length() + 6 + ((brackets.size() + 1)));
+                        json = json.substring(0, x - ((brackets.size() + 1)) - 2) + "[\n" + json.substring(x - ((brackets.size() + 1)) - 2);
+                        j += 2;
+                        brackets.pop();
+                        brackets.push("]");
+                        //return old value of x
+                        if (!ind.isEmpty()) {
+                            ind.pop();
+                            if (!ind.isEmpty()) {
+                                x = ind.peek();
+                            }
+                        }
+                        //skip new <topic>
+                        i++;
+                        stack_open_tags.push(word);
+                        tag.push(word);
+                        opening_tag.push(true);
+                        json += "\n";
+                        j += 1;
+                        word = "";
+                        continue;
+                    }//condition for the first opening tag bec stk is still empty
+                    if (stack_open_tags.isEmpty()) {        //
+                        json += "\n";
+                    }//if last thing in json was closing tag, no need to open "{"
+                    else if (opening_tag.peek() == false) {   //if it is opening tag or we have closed the tag before it
+
+                        json += "\n";
+                        j += 1;
+                    } else { //for formating                                   //if it is closing tag or the last opened tag is not closed yet
+                        for (int t = brackets.size() + 1; t > 0; t--) {
+                            json += " ";
+                            j += 1;
+                        }
+                        json += "{\n";
+                        j += 2;
+                        //assuming if there is array in xml file, it must be writtin as (users,user - topics,topic)
+                        //save index of user in json to be deleted if it's repeated in the same scope of users
+                        if ((word + "s").equals(stack_open_tags.peek())) {
+                            x = j + brackets.size() + 3; //x->"
+                            ind.push(x);
+                        }
+                        brackets.push("}");     //add closing curly bracket for the one we added
+                    }//for formating
+                    for (int t = brackets.size() + 1; t > 0; t--) {
+                        json += " ";
+                        j += 1;
+                    }
+                    json += "\"";
+                    j++;
+                    json += word;
+                    j += word.length();     //write tag name in json file
+                    json += "\": ";
+                    j += 3;
+                    stack_open_tags.push(word);     //store the opened tag in the stack
+                    tag.push(word);
+                    i++;
+                    opening_tag.push(true);     //we have opened new tag
+                } else {                        //if it is closing tag
+                    while (!w.equals(">")) {    //store the closing tag in word
+                        i++;
+                        w = s.substring(i, i + 1);
+                        word += w;
+                    }
+                    word = word.substring(0, word.length() - 1);    //to remove the > from the end of the word
+                    //opening tags are pushed into tag*stack* and poped when they are closed
+                    //before poping they are stored in tags*string*
+                    //if opening tag is repeated after the closing tag, it will be deleted from json*string*
+                    if (word.equals(tag.peek())) {  //check the last tag
+                        last_closed_tag = word;     //store the last closed tag
+                        tag.pop();                  //pop the tag that is closed
+
+                        // //"topic", x=90->", topics length = count = 6,
+                        int count = last_closed_tag.length();
+                        String currentWord;
+                        currentWord = json.substring(x + 1, x + 2);
+                        //currentWord = t
+
+                        //if scope of topics ended without repeating topic, remove index of topic.
+                        for (int L = x + 2; L < x + count; L++) {
+                            currentWord += json.substring(L, L + 1); //topic
+                        }
+
+                        //topic + s == topics
+                        if ((currentWord + "s").equals(last_closed_tag)) {
+                            //pop 90
+                            ind.pop();
+                            //x = index of post
+                            if (!ind.isEmpty()) {
+                                x = ind.peek();
+                            }
+                        }
+                    }
+                    //print closing brackets
+                    if (opening_tag.peek() == false) {
+                        String back;
+                        back = json.substring(json.length() - 1);
+
+                        //removing unwanted commas
+                        if ((brackets.peek().equals("]") && bracket.equals("}")) ||
+                                (brackets.peek().equals("}") && back.equals(",")) ||
+                                (brackets.peek().equals("]") && back.equals(","))) { //brackets.top()-> barcket to be printed, bracket->last printed bracket
+                            json = json.substring(0, json.length() - 1);
+                            j--;
+                        }
+                        json += "\n";
+                        j += 1;
+                        for (int t = brackets.size(); t > 0; t--) {
+                            json += " ";
+                            j += 1;
+                        }
+                        json += brackets.peek();
+                        bracket = brackets.peek();
+                        j++;
+                        brackets.pop();
+                        if (!brackets.isEmpty()) {
+                            json += ",";
+                            j++;
+                        }
+                    }
+                    i++;
+                    opening_tag.push(false);    //last opened tag is closed
+                }
+            }
+            //data to be printed char by char so no need for word here
+            else {
+                json += "\"";
+                j++;
+                while (!w.equals("<")) {    //add the data to the json until find an opening tag
+                    json += w;
+                    j++;
+                    i++;
+                    w = s.substring(i, i + 1);
+                }
+                json += "\"";   //close the data field
+                json += ",";    // add another
+                j += 2;
+            }
+            word = "";
+        }
+
+        json += "\n}";  //closing json string
+        j += 2;
+        this.StringData=json;
+        return json;
+    }
+    //----------------------------------------------------------
+    // Minify XML file by removing spaces an new lines
+    public String Minify() {
+        undo.push(this.StringData);
+        String file=this.StringData;
+        StringBuilder file_after_minify= new StringBuilder();
+        String[] lines = split_the_XML_file(file);
+        for (int i =0 ; i<lines.length ; i++){
+
+            if (lines[i].trim().length()==0)
+            {
+                continue;
+            }
+            lines[i] = lines[i].trim();
+            file_after_minify.append(lines[i].trim());
+        }
+        this.StringData=file_after_minify.toString();
+        return file_after_minify.toString();
+
     }
 
 
     //----------------------------------------------------------
+    // Format XML File
     public  String Prettify(){
         undo.push(this.StringData);
         String file=this.StringData;
         StringBuilder file_after_format= new StringBuilder();
 
         int n=0;
-//file is empty
-//        if (file.trim().length()==0)
-//        {
-//            return "file is Empty";
-//        }
+
         String[] lines = split_the_XML_file(file);
 
         for (int i =0 ; i<lines.length ; i++) {
@@ -275,14 +457,17 @@ public class MainFiles {
 
         return  file_after_format.toString();
     }
+    //----------------------------------------------------------
+    // Split XML file to be used in Minify() and Prettify()
     private  String[] split_the_XML_file(String File){
         String[] lines;
         File =File.trim().replaceAll("<", "\n<").replaceAll(">", ">\n");
         lines = File.split("\n");
         return lines;
     }
+    //----------------------------------------------------------
 
-    // function to generate Indentation;
+    // function to generate Indentation to be used in Prettify()
     private String generateIndentation(int level)
     {
         StringBuilder indention = new StringBuilder();
@@ -293,29 +478,33 @@ public class MainFiles {
         return indention.toString();
     }
 
+    //----------------------------------------------------------
+    //Undo
     public String Undo(){
-
+        //Check if undo stack is empty return null
         if (undo.empty()){
             return null;
         }
         else{
-            redo.push(this.StringData);
-            this.StringData=undo.peek();
+            redo.push(this.StringData); // push the top of undo stack in the redo stack
+            this.StringData=undo.peek(); // put the top of undo stack in StringData then pop
             undo.pop();
             return this.StringData;
         }
 
     }
+    //----------------------------------------------------------
+    //Redo
     public String Redo(){
-
+        //Check if redo stack is empty return null
         if (redo.empty()){
 
             return  null;
         }
         else{
 
-            this.StringData=redo.peek();
-            undo.push(redo.peek());
+            this.StringData=redo.peek();// push the top of redo stack in the undo stack
+            undo.push(redo.peek());  // put the top of redo stack in StringData then pop
             redo.pop();
             return this.StringData;
         }
