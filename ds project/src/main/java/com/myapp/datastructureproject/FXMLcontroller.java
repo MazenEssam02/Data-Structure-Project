93d05f59a990ea2c54aab554848f1e0921d702ac
tasneem-elhady <127930144+tasneem-elhady@users.noreply.github.com>
2023-12-30 00:49:16 +0200

    Update FXMLcontroller.java

--- ds project/src/main/java/com/myapp/datastructureproject/FXMLcontroller.java
+++ ds project/src/main/java/com/myapp/datastructureproject/FXMLcontroller.java
@@ -19,10 +19,7 @@ import com.myapp.datastructureproject.networkAnalysis.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
 
 import javafx.stage.FileChooser;
@@ -77,15 +74,21 @@ public class FXMLcontroller {
             TextField user_s;
     @FXML
             Button suggest_btn;
-
+    @FXML
+            RadioButton compressed_mode;
+    @FXML
+            Button save_btn;
+//    GUI
     Alert Alarm;
     Network_nodes network_nodes;
     Operation_nodes operation_nodes;
+//    network analysis
     NetworkHelper networkHelper;
     com.myapp.datastructureproject.networkAnalysis.Graph graph;
     User [] users_list;
     MapF users_map;
     User [][] users_suggest;
+//    main files object
     MainFiles content;
 
     @FXML
@@ -97,6 +100,7 @@ public class FXMLcontroller {
         network_nodes = new Network_nodes(u1,u2,topic,find_mutual_btn,search_btn,visualize_btn,find_most_followd_btn,find_most_active_btn,suggest_btn,user_s);
         operation_nodes = new Operation_nodes(pretify_btn,json_btn,minify_btn,compress_btn,decompress_btn,analyse_btn,undo_btn,redo_btn);
         FXMLhelper.disable(correct_btn);
+        FXMLhelper.disable(save_btn);
 
 //    listener on action when text in text area changes
         fixed_text_area.textProperty().addListener((obs, oldText, newText) -> {
@@ -104,9 +108,26 @@ public class FXMLcontroller {
             operation_nodes.enable(false);
             network_nodes.enable(false);
             FXMLhelper.disable(correct_btn);
+            FXMLhelper.disable(save_btn);
             FXMLhelper.enable(check_btn);
+//          reset compressed mode button
+            compressed_mode.setSelected(false);
+            FXMLhelper.enable(compressed_mode);
 //          reset view operation text area
             view_operation.setText("");
+        });
+//        listener to compressed mode radio button
+        compressed_mode.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            //if RadioButton is selected
+            if (newValue) {
+                //check if RadioButton is selected
+                FXMLhelper.enable(decompress_btn);
+                FXMLhelper.disable(check_btn);
+            }
+            else {
+                FXMLhelper.disable(decompress_btn);
+                FXMLhelper.enable(check_btn);
+            }
         });
 
     }
@@ -132,9 +153,8 @@ public class FXMLcontroller {
                         stringBuilder.append(line).append("\n");
                     }
                 } catch (IOException e) {
-                    Alarm.setContentText("An error occurred while reading the file:" );
+                    Alarm.setContentText("xml file path not correct");
                     Alarm.show();
-                    System.out.println("An error occurred while reading the file: " + e.getMessage());
                 }
                 fixed_text_area.setText(fileContent);
 //                fixed_text_area.setEditable(false);
@@ -157,7 +177,7 @@ public class FXMLcontroller {
             FileChooser fc = new FileChooser();
 
             fc.setTitle("choose file");
-            fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("XML Files", new String[]{"*xml"}));
+            fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("XML Files", "*xml"));
             File file = fc.showOpenDialog(null);
             path.setText(file.getAbsolutePath());
         }catch (Exception e){
@@ -167,11 +187,10 @@ public class FXMLcontroller {
     //------------------------------------------------------------------------------
     //on action of check button
     public void check(ActionEvent ev) {
-        ArrayList data = FXMLhelper.splitMultilineString(fixed_text_area.getText());
+        ArrayList<String> data = FXMLhelper.splitMultilineString(fixed_text_area.getText());
         if (fixed_text_area.getText().length() == 0)
             return;
         content = new MainFiles(data);
-
         String valid = FXMLhelper.isValid(content);
         if(valid.length() == 0) {
             view_operation.setText("The xml file has no missing tags");
@@ -182,14 +201,21 @@ public class FXMLcontroller {
             view_operation.setText(valid);
             FXMLhelper.enable(correct_btn);
         }
+        FXMLhelper.disable(compressed_mode);
     }
     //------------------------------------------------------------------------------
     //on action of correct button
     public void correct(ActionEvent ev) {
         view_operation.setText(content.correction());
+        FXMLhelper.enable(save_btn);
     }
     //------------------------------------------------------------------------------
-    //on action of undo button
+    //on action of Save button
+    public void save(ActionEvent ev){
+        fixed_text_area.setText(view_operation.getText());
+    }
+    //------------------------------------------------------------------------------
+    //on action of prettify button
     public void prettify(ActionEvent ev) {
         view_operation.setText(content.Prettify());
     }
@@ -205,14 +231,13 @@ public class FXMLcontroller {
     //------------------------------------------------------------------------------
     //on action of compress button
     public void compress(ActionEvent ev) {
-        view_operation.setText("compressed");
+        view_operation.setText(content.Compress());
     }
     //------------------------------------------------------------------------------
     //on action of decompress button
     public void decompress(ActionEvent ev) {
-        view_operation.setText("decompressed");
+        view_operation.setText(content.Decompress());
     }
-
     //on action of undo button
     public void undo(ActionEvent ev) {
         view_operation.setText(content.Undo());
@@ -285,10 +310,14 @@ public class FXMLcontroller {
     //------------------------------------------------------------------------------
     //on action of find mutual button
     public void Find_mutual(ActionEvent ev){
-        String user1 = u1.getText();
-        String user2 = u2.getText();
+        String user1 = u1.getText().replaceAll("\\s", "");
+        String user2 = u2.getText().replaceAll("\\s", "");
         if(user1.length() == 0 || user2.length() == 0)
             return;
+        else if(!FXMLhelper.isValidID(user1, users_list) ||!FXMLhelper.isValidID(user2, users_list)) {
+            Alarm.setContentText("Please enter a valid ID!");
+            Alarm.show();
+        }
         else{
             view_operation.setText(Arrays.toString(networkHelper.mutualFollowers(user1,user2)));
         }
@@ -306,13 +335,15 @@ public class FXMLcontroller {
     //------------------------------------------------------------------------------
     //on action of search button
     public void search(ActionEvent ev){
-                view_operation.setText(networkHelper.search(topic.getText()));
+        if(topic.getText().length() == 0 || topic.getText().trim().length() == 0)
+            return;
+        view_operation.setText(networkHelper.search(topic.getText().trim()));
     }
     //------------------------------------------------------------------------------
     //on action of suggest button
     public void suggest(ActionEvent ev){
         String u = user_s.getText().replaceAll("\\s", "");
-        if(FXMLhelper.isValidID(u, users_list.length)) {
+        if(FXMLhelper.isValidID(u, users_list)) {
             int user_index = users_map.get(u);
             view_operation.setText("for" + users_list[user_index].toString() + "\nsuggested: " + Arrays.toString(users_suggest[user_index]));
         }
