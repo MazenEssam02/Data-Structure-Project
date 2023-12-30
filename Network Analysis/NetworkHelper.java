8dd65d3eda7cfb852020c65a959794afb3f12398
AlaaMohsen231 <131255063+AlaaMohsen231@users.noreply.github.com>
2023-12-30 11:34:16 +0200

    Update mutual followers

--- Network Analysis/NetworkHelper.java
+++ Network Analysis/NetworkHelper.java
@@ -130,40 +130,22 @@ return suggestionUser;   }
 
 
 
-public  User[] mutualFollowers(String ID1 , String ID2 ){
-    MapF mapF = createUserIndexMap();
-    Graph graph = CreateNetwork();
-    int user1 = mapF.get(ID1);
-    int user2 = mapF.get(ID2);
-int [] notMutual = new int[users.length];
+public  ArrayList<User> mutualFollowers(String ID1 , String ID2 ){
+        MapF mapF = createUserIndexMap();
+        Graph graph = CreateNetwork();
+        int user1 = mapF.get(ID1);
+        int user2 = mapF.get(ID2);
 
-    for(int i=0;i<users.length;i++){
-         if(!(graph.isConnected(user1,i) && graph.isConnected(user2,i))){
-             notMutual[i]=1;
-         }
-    }
-    int counter = 0 ;
+        ArrayList<User> mutualUsers=new ArrayList<>();
 
-    for(int i=0;i<users.length;i++){
-        if(notMutual[i] != 1){ counter++;
+        for(int i=0;i<users.length;i++){
+            if((graph.isConnected(user1,i) && graph.isConnected(user2,i))){
+                mutualUsers.add(users[i]);
+            }
         }
-    }
-    User [] mutualUsers = new User[counter];
-
-    int j =0;
-    for(int i=0;i<users.length;i++){
-         if(notMutual[i] != 1){
-             mutualUsers[j]=users[i];
-             j++;
-         }
-    }
-
-
-
-
 
  return mutualUsers;
-        }
+    }
     
 public User mostFollowed() {
     int maxFollowers = users[0].Followers.size();
